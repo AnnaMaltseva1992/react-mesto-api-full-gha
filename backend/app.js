2c203727e9a73cb6742eb6a53ce189c0bf62aecd
Anna Maltseva <mr-sbazin@yandex.ru>
2023-08-15 15:58:33 +0300

    добавлен logger

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,8 @@ const router = require('./routes');
 
 const handleErrors = require('./middlewares/handleErrors');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
@@ -32,7 +34,15 @@ app.use(helmet());
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(handleErrors);
 
