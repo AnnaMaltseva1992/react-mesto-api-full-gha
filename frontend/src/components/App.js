d07b486814dae16542c8613aafb4fd3ab3715d73
Anna Maltseva <mr-sbazin@yandex.ru>
2023-08-18 17:33:33 +0300

    сохранение авторизации

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -74,7 +74,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
     api
       .changeLikeCardStatus(card._id, isLiked)
       .then((newCard) => {
@@ -196,6 +196,13 @@ function App() {
   useEffect(() => {
     tokenCheck();
   }, []);
+
+  useEffect(() => {
+    if (loggedIn === true) {
+        navigate('/main')
+    }
+}, [loggedIn, navigate]);
+
 
   function handleSignOut() {
     localStorage.removeItem("jwt");
